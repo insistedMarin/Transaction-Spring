7abd8193b10dcba4f0636cdc732392deaa550286
Yang Wong <yang wang>
2023-12-01 19:57:23 +0100

    add ui,python script

--- ui/src/main/java/org/example/exchange/UIApplication.java
+++ ui/src/main/java/org/example/exchange/UIApplication.java
@@ -1,12 +1,41 @@
 package org.example.exchange;
 
+import org.example.exchange.client.RestClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UIApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UIApplication.class, args);
+    }
+
+    @Bean
+    public RestClient createRestClient(
+            @Value("${exchange.config.api-endpoints.trading-api}") String tradingApiEndpoint,
+            @Autowired ObjectMapper objectMapper) {
+        return new RestClient.Builder(tradingApiEndpoint).build(objectMapper);
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            /**
+             * Keep "/static/" prefix
+             */
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+            }
+        };
     }
 }
